e29491fc3a14ca92472a97c6dcd111647a5b5774
corcorasgit <corcoras.git@protonmail.com>
2025-06-21 14:31:14 -0700

    day9 classes

---
+++ day9.py
@@ -0,0 +1,18 @@
+
+obj = MessageUser()
+obj.add_user("Justin", 123.12, email="hello@teamcfe.com")
+obj.add_user("john", 94.23)
+obj.add_user("shaun", 93.23)
+obj.add_user("rene", 99.99)
+obj.add_user("rod", 45.22)
+obj.add_user("mikeala", 56.22)
+obj.add_user("liam", 11.22)
+
+obj.make_messages
+
+
+
+
+            
+            
+
